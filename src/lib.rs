20030bab1f4534030cb5ccf5e32119c992bf50b2
John Arundel <john@bitfieldconsulting.com>
2024-04-27 17:58:01 +0100

    more tests

--- src/lib.rs
+++ src/lib.rs
@@ -25,8 +25,65 @@ impl Store {
     }
 }
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Eq, PartialOrd, Ord)]
 pub struct Pair {
     key: &'static str,
     value: &'static str,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_store_contains_no_data() {
+        let s = Store::new();
+        assert!(s.all().is_empty(), "unexpected data found in new store")
+    }
+
+    #[test]
+    fn get_nonexistent_key_returns_none() {
+        let s = Store::new();
+        assert!(s.get("bogus").is_none(), "unexpected value found for bogus")
+    }
+
+    #[test]
+    fn get_existing_key_returns_expected_value() {
+        let mut s = Store::new();
+        s.set("foo", "bar");
+        assert_eq!(s.get("foo"), Some("bar"), "no value found for existing key")
+    }
+
+    #[test]
+    fn set_same_key_overwrites_previous_value() {
+        let mut s = Store::new();
+        s.set("foo", "old");
+        s.set("foo", "new");
+        assert_ne!(
+            s.get("foo"),
+            Some("old"),
+            "old value not overwritten by new"
+        );
+        assert_eq!(s.get("foo"), Some("new"), "no value found for existing key")
+    }
+
+    #[test]
+    fn all_returns_all_pairs() {
+        let mut s = Store::new();
+        s.set("foo", "bar");
+        s.set("baz", "quux");
+        let want = vec![
+            Pair {
+                key: "baz",
+                value: "quux",
+            },
+            Pair {
+                key: "foo",
+                value: "bar",
+            },
+        ];
+        let mut got = s.all();
+        got.sort();
+        assert_eq!(want, got, "expected data not returned")
+    }
 }
--- tests/tests.rs
+++
@@ -1,8 +0,0 @@
-mod tests {
-    #[test]
-    fn new_store_contains_no_data() {
-        let s = rsk::Store::new();
-        let empty = Vec::new();
-        assert_eq!(s.all(), empty)
-    }
-}
