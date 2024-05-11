d02ef7d288872071dd2b22cb94f8459219d5ccdb
John Arundel <john@bitfieldconsulting.com>
2024-05-11 16:39:23 +0100

    use string, string

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,43 @@
+use serde::Deserialize;
 use std::collections::{hash_map, HashMap};
 use std::fs::File;
-use std::io::ErrorKind;
+use std::io::BufReader;
 use std::path::Path;
 
-#[derive(Debug)]
-pub struct Store<'a> {
-    data: HashMap<&'a str, &'a str>,
+#[derive(Debug, Deserialize)]
+pub struct Store {
+    data: HashMap<String, String>,
 }
 
-impl<'a> Store<'a> {
+impl Store {
     pub fn open_or_create<P: AsRef<Path>>(path: P) -> Result<Self, std::io::Error> {
         match File::open(&path) {
-            Ok(_) => Ok(Self {
-                data: HashMap::new(),
-            }),
-            Err(e) if e.kind() == ErrorKind::NotFound => Ok(Self {
+            Ok(file) => {
+                let reader = BufReader::new(file);
+                let data = serde_json::from_reader(reader)?;
+                Ok(Self { data })
+            }
+            Err(e) if e.kind() == std::io::ErrorKind::NotFound => Ok(Self {
                 data: HashMap::new(),
             }),
             Err(e) => Err(e),
         }
     }
 
-    pub fn set(&mut self, k: &'a str, v: &'a str) -> Option<&str> {
-        self.data.insert(k, v)
+    pub fn save(&self) -> Result<(), std::io::Error> {
+        Ok(())
     }
 
-    pub fn get(&self, k: &str) -> Option<&str> {
-        self.data.get(k).copied()
+    pub fn set(&mut self, k: &str, v: &str) -> Result<(), std::io::Error> {
+        self.data.insert(k.to_string(), v.to_string());
+        self.save()
     }
 
-    pub fn iter(&self) -> hash_map::Iter<'a, &str, &str> {
+    pub fn get(&self, k: &str) -> Option<&String> {
+        self.data.get(k)
+    }
+
+    pub fn iter(&self) -> hash_map::Iter<String, String> {
         self.data.iter()
     }
 }
@@ -45,7 +53,7 @@ mod tests {
     fn new_store_contains_no_data() {
         let s = new_test_store();
         assert_eq!(
-            Vec::<(&&str, &&str)>::new(),
+            Vec::<(&String, &String)>::new(),
             s.iter().collect::<Vec<_>>(),
             "unexpected data found in new store"
         )
@@ -60,31 +68,44 @@ mod tests {
     #[test]
     fn get_returns_expected_value_for_existing_key() {
         let mut s = new_test_store();
-        s.set("foo", "bar");
-        assert_eq!(Some("bar"), s.get("foo"), "get returned unexpected result")
+        s.set("foo", "bar").unwrap();
+        assert_eq!(
+            &"bar".to_string(),
+            s.get("foo").unwrap(),
+            "get returned unexpected result"
+        )
     }
 
     #[test]
     fn set_same_key_fn_overwrites_old_value_and_returns_it() {
         let mut s = new_test_store();
-        s.set("foo", "old");
-        let old = s.set("foo", "new").unwrap();
-        assert_eq!("old", old);
+        s.set("foo", "old").unwrap();
+        s.set("foo", "new").unwrap();
         assert_eq!(
-            Some("new"),
-            s.get("foo"),
+            &"new".to_string(),
+            s.get("foo").unwrap(),
             "old value not overwritten by new"
         );
-        assert_eq!(s.get("foo"), Some("new"), "no value found for existing key")
+        assert_eq!(
+            s.get("foo").unwrap(),
+            &"new".to_string(),
+            "no value found for existing key"
+        )
     }
 
     #[test]
     fn store_contains_expected_data() {
         let mut s = new_test_store();
-        s.set("foo", "bar");
-        s.set("baz", "quux");
-        let want = vec![(&"baz", &"quux"), (&"foo", &"bar")];
-        let mut data: Vec<(&&str, &&str)> = s.iter().collect();
+        s.set("foo", "bar").unwrap();
+        s.set("baz", "quux").unwrap();
+        let (baz, quux, foo, bar) = (
+            String::from("baz"),
+            String::from("quux"),
+            String::from("foo"),
+            String::from("bar"),
+        );
+        let want = vec![(&baz, &quux), (&foo, &bar)];
+        let mut data = s.iter().collect::<Vec<_>>();
         data.sort();
         assert_eq!(want, data, "expected data not returned")
     }
@@ -102,15 +123,13 @@ mod tests {
         fs::write(&path, "").unwrap();
         // 'TMPDIR/not_a_directory/store_file' is invalid
         // because 'not_a_directory' is not a directory
-        let s = Store::open_or_create(&path.join("store_file"));
+        let s = Store::open_or_create(path.join("store_file"));
         assert!(s.is_err(), "want error for invalid path")
     }
 
-    fn new_test_store<'a>() -> Store<'a> {
+    fn new_test_store() -> Store {
         Store {
             data: HashMap::new(),
         }
     }
-
-    // dummy change 2
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,7 @@
 use rsk::Store;
 
 fn main() {
-    let mut s = Store::open_or_create("store.tmp").unwrap();
-    s.set("foo", "bar");
-    let v = s.get("bogus");
-    match v {
-        Some(v) => println!("{}", v),
-        None => println!("not found"),
-    }
-    for (k, v) in s.iter() {
-        println!("{k}: {v}")
-    }
+    let mut s = Store::open_or_create("store.kv").unwrap();
+    s.set("foo", "bar").unwrap();
+    s.iter().for_each(|(k, v)| println!("{k}: {v}"));
 }
