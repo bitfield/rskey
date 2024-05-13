6be5279f734579591d9893f16f669699da6ea543
John Arundel <john@bitfieldconsulting.com>
2024-05-13 13:37:41 +0100

    disable error test on windows

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 use std::collections::{hash_map, HashMap};
 use std::ffi::OsString;
 use std::fs::File;
-use std::io::{BufReader, BufWriter};
+use std::io::{BufReader, BufWriter, ErrorKind};
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Store {
@@ -11,24 +11,32 @@ pub struct Store {
 }
 
 impl Store {
+    /// # Errors
+    ///
+    /// Will return `Err` for any error opening the file other than
+    /// [`ErrorKind::NotFound`].
     pub fn open_or_create(path: &OsString) -> Result<Self, std::io::Error> {
         match File::open(path) {
             Ok(file) => {
                 let reader = BufReader::new(file);
                 let data = serde_json::from_reader(reader)?;
                 Ok(Self {
-                    path: path.to_os_string(),
+                    path: path.clone(),
                     data,
                 })
             }
-            Err(e) if e.kind() == std::io::ErrorKind::NotFound => Ok(Self {
-                path: path.to_os_string(),
+            Err(e) if e.kind() == ErrorKind::NotFound => Ok(Self {
+                path: path.clone(),
                 data: HashMap::new(),
             }),
             Err(e) => Err(e),
         }
     }
 
+    /// # Errors
+    ///
+    /// Will return `Err` for any error creating the file or serializing the
+    /// JSON to it.
     pub fn save(&self) -> Result<(), std::io::Error> {
         let file = File::create(&self.path)?;
         let writer = BufWriter::new(file);
@@ -36,18 +44,31 @@ impl Store {
         Ok(())
     }
 
+    /// # Errors
+    ///
+    /// Will return any `Err` encountered by [`Self::save`].
     pub fn set(&mut self, k: &str, v: &str) -> Result<(), std::io::Error> {
         self.data.insert(k.to_string(), v.to_string());
         println!("Saving {:?}", self.path);
         self.save()
     }
 
+    #[must_use]
     pub fn get(&self, k: &str) -> Option<&String> {
         self.data.get(k)
     }
 
+    #[must_use]
     pub fn iter(&self) -> hash_map::Iter<String, String> {
         self.data.iter()
+    }
+}
+
+impl<'a> IntoIterator for &'a Store {
+    type IntoIter = std::collections::hash_map::Iter<'a, std::string::String, std::string::String>;
+    type Item = (&'a std::string::String, &'a std::string::String);
+    fn into_iter(self) -> Self::IntoIter {
+        self.iter()
     }
 }
 
@@ -126,12 +147,11 @@ mod tests {
     }
 
     #[test]
+    #[cfg(not(windows))] // can't simulate a non-NotFound error on Windows
     fn open_or_create_fn_errors_on_invalid_path() {
         let tmp_dir = TempDir::new().unwrap();
         let path = tmp_dir.path().join("not_a_directory");
         fs::write(&path, "").unwrap();
-        // 'TMPDIR/not_a_directory/store_file' is invalid
-        // because 'not_a_directory' is not a directory
         let store_path = path.join("store_file").into_os_string();
         let s = Store::open_or_create(&store_path);
         assert!(s.is_err(), "want error for invalid path, got {:?}", s)
@@ -140,7 +160,6 @@ mod tests {
     fn new_test_store() -> (TempDir, Store) {
         let tmp_dir = TempDir::new().unwrap();
         let path = tmp_dir.path().join("store.kv").into_os_string();
-        println!("about to save {:?}", path);
         File::create(&path).unwrap();
         (
             tmp_dir,
